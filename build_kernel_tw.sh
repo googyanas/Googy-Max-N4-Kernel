6d48c391ef59dda36e92c8cb55f88684ee2e6869
googyanas <googyanas@gmail.com>
2015-08-14 00:30:46 +0100

    1st attempt to build own dt.img

--- build_kernel_tw.sh
+++ build_kernel_tw.sh
@@ -21,9 +21,10 @@ make ARCH=arm 0googymax_exynos5433-trelte_defconfig || exit 1
 
 cd $KERNELDIR/
 
+make exynos5433-tre_eur_open_16.dtb || exit 1
 
 make -j4 zImage || exit 1
-tools/dtbTool -o dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/dts/ || exit 1
+tools/dtbToolCM -o dt.img -s 2048 -p scripts/dtc/ arch/arm/boot/dts/ || exit 1
 
 # make -j4 || exit 1
 
